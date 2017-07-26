487eac00a12fa8d0031c42b04f900048b897afd3
Rafał Trzop <rafaltrzop@gmail.com>
2017-07-26 21:15:07 +0200

    Basic Jasmine configuration

--- bin/knapsack-pro.js
+++ bin/knapsack-pro.js
@@ -1,3 +1,44 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+var Jasmine = require('jasmine');
+var jasmine = new Jasmine();
+
+jasmine.loadConfig({
+  spec_dir: 'spec',
+  spec_files: [
+    '**/*[sS]pec.js'
+  ],
+  helpers: [
+    'helpers/**/*.js'
+  ]
+});
+
+jasmine.onComplete(function(passed) {
+  if (passed) {
+    console.log('All specs have passed');
+  } else {
+    console.log('At least one spec has failed');
+  }
+});
+
+var myReporter = {
+  jasmineStarted: function(suiteInfo) {
+    console.log(suiteInfo);
+  },
+  specStarted: function(result) {
+    console.log(result);
+  },
+  specDone: function(result) {
+    console.log(result);
+  },
+  suiteStarted: function(result) {
+    console.log('suite started');
+  },
+  suiteDone: function(result) {
+    console.log('suite done');
+  }
+};
+
+jasmine.addReporter(myReporter);
+
+jasmine.execute();
