aafc0c02e6dbf7fec102014a761cb171fda22426
Rafał Trzop <rafaltrzop@gmail.com>
2017-07-26 22:40:45 +0200

    Display spec file name

--- bin/knapsack-pro.js
+++ bin/knapsack-pro.js
@@ -23,22 +23,28 @@ jasmine.onComplete(function(passed) {
 
 var myReporter = {
   jasmineStarted: function(suiteInfo) {
-    console.log(suiteInfo);
+    // console.log(suiteInfo);
   },
   specStarted: function(result) {
-    console.log(result);
+    // console.log(result);
   },
   specDone: function(result) {
-    console.log(result);
+    // console.log(result);
+    // console.log(jasmine.env);
   },
   suiteStarted: function(result) {
-    console.log('suite started');
+    // console.log('suite started');
   },
   suiteDone: function(result) {
-    console.log('suite done');
+    // console.log('suite done');
+    // const util = require('util');
+    // console.log(util.inspect(jasmine.env, true, null));
   }
 };
 
 jasmine.addReporter(myReporter);
+
+console.log(jasmine.specFiles);
+// console.log(jasmine.env);
 
 jasmine.execute();
