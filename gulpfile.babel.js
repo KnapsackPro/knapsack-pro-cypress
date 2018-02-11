4bbc6ea48025efee4783c0a12b01f70df11eb515
Rafał Trzop <rafaltrzop@gmail.com>
2018-02-11 15:11:37 +0100

    Set up gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,34 @@
+import gulp from 'gulp';
+import del from 'del';
+import ts from 'gulp-typescript';
+
+const tsProject = ts.createProject('tsconfig.json');
+const paths = {
+  src: tsProject.config.include,
+  dest: tsProject.config.compilerOptions.outDir,
+};
+
+export function clean() {
+  return del([
+    `${paths.dest}/**`,
+    `!${paths.dest}`
+  ]);
+}
+
+export function watch() {
+  gulp.watch(paths.src, compile);
+}
+
+export function compile() {
+  return tsProject.src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest(paths.dest));
+}
+
+export const build = gulp.series(
+  clean,
+  compile,
+  watch
+);
+
+export default build;
