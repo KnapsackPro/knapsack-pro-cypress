b7bc4901a71c09be76ee6bcef7b7fc507796d89e
ArturT <arturtrzop@gmail.com>
2018-08-04 22:09:01 +0200

    Update onSuccess callback to return promise for deferred recorded test files

--- src/knapsack-pro-cypress.ts
+++ src/knapsack-pro-cypress.ts
@@ -5,8 +5,14 @@ import { TestFile } from '@knapsack-pro/core/test-file.model';
 
 const knapsackPro = new KnapsackProCore([]);
 knapsackPro.runQueueMode((queueTestFiles: TestFile[]) => {
+  const recordedTestFiles<TestFile[]> = [];
+  const deferredRecordedTestFiles = new Promise()
+
   // run tests by cypress
-  // return recorded timing for those tests
+  // https://docs.cypress.io/guides/guides/command-line.html#Cypress-Module-API
+  deferredRecordedTestFiles.resolve(recordedTestFiles);
+
+  return deferredRecordedTestFiles;
 }, (error: any) => {
 
 });
