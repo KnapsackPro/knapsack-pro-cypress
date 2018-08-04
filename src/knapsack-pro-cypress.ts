15ab8d4e9755c169690914576676208b95328c2f
ArturT <arturtrzop@gmail.com>
2018-08-04 18:40:41 +0200

    Pass onSuccess and onFailure callbacks

--- src/knapsack-pro-cypress.ts
+++ src/knapsack-pro-cypress.ts
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
 import KnapsackProCore = require('@knapsack-pro/core');
+import { TestFile } from '@knapsack-pro/core/test-file.model';
 
 const knapsackPro = new KnapsackProCore([]);
-knapsackPro.runQueueMode();
+knapsackPro.runQueueMode((queueTestFiles: TestFile[]) => {
+  // run tests by cypress
+  // return recorded timing for those tests
+}, (error: any) => {
+
+});
