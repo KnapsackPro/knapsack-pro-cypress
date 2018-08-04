ed97652033a223d81f369f40960116fab36b4251
ArturT <arturtrzop@gmail.com>
2018-08-04 18:40:23 +0200

    Use name @knapsack-pro/core for dependency

--- src/knapsack-pro-cypress.ts
+++ src/knapsack-pro-cypress.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import KnapsackProCore = require('knapsack-pro-core');
+import KnapsackProCore = require('@knapsack-pro/core');
 
 const knapsackPro = new KnapsackProCore([]);
 knapsackPro.runQueueMode();
