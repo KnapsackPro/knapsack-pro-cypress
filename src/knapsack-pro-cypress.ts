0cf1594d1dc47ea93f4a84a1868766ac09eaebf7
ArturT <arturtrzop@gmail.com>
2018-08-05 13:46:00 +0200

    Try to import core

--- src/knapsack-pro-cypress.ts
+++ src/knapsack-pro-cypress.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import KnapsackProCore = require("@knapsack-pro/core");
+import KnapsackProCore from "@knapsack-pro/core";
 import { TestFile } from "@knapsack-pro/core/test-file.model";
 
 const knapsackPro = new KnapsackProCore([]);
