2ecd69ba3ef99f8d7bf8910215bf317f45795953
ArturT <arturtrzop@gmail.com>
2018-08-14 17:55:20 +0200

    Install cypress as dependency
    Update @types/jquery to fix bug with
    https://github.com/DefinitelyTyped/DefinitelyTyped/issues/28000#issuecom
    ment-411824529
    
    Add dom to lib in tsconfig to fix problem with compilation when cypress
    imported.
    
    Extract onSuccess and onError callbacks.

--- src/knapsack-pro-cypress.ts
+++ src/knapsack-pro-cypress.ts
@@ -1,11 +1,23 @@
 #!/usr/bin/env node
 
 import { KnapsackProCore, TestFile } from "@knapsack-pro/core";
+import "cypress";
 
 const allTestFiles: TestFile[] = [];
 const knapsackPro = new KnapsackProCore(allTestFiles);
-knapsackPro.runQueueMode((queueTestFiles: TestFile[]) => {
+
+const onSuccess = async (queueTestFiles: TestFile[]) => {
   const recordedTestFiles: TestFile[] = [];
+
+  // cypress.run({
+  //   spec: './cypress/integration/examples/actions.spec.js'
+  // })
+  // .then((results) => {
+  //   console.log(results)
+  // })
+  // .catch((err) => {
+  //   console.error(err)
+  // })
 
   const deferredRecordedTestFiles = new Promise<TestFile[]>((resolve, reject) => {
     // run tests by cypress
@@ -14,6 +26,10 @@ knapsackPro.runQueueMode((queueTestFiles: TestFile[]) => {
   });
 
   return deferredRecordedTestFiles;
-}, (error: any) => {
+};
+
+const onError = (error: any) => {
   // handle error
-});
+};
+
+knapsackPro.runQueueMode(onSuccess, onError);
