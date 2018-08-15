558ac83ce3e2a336b5368fab620b1a4d7821e523
ArturT <arturtrzop@gmail.com>
2018-08-15 16:51:40 +0200

    Add glob and @types/glob. Detect test file paths

--- src/knapsack-pro-cypress.ts
+++ src/knapsack-pro-cypress.ts
@@ -2,13 +2,43 @@
 
 import { KnapsackProCore, TestFile } from "@knapsack-pro/core";
 const cypress = require("cypress"); // tslint:disable-line:no-var-requires
+const glob = require("glob");
 
-const allTestFiles: TestFile[] = [{path: "a_spec.rb"}];
-// const allTestFiles: TestFile[] = [];
-const knapsackPro = new KnapsackProCore(allTestFiles);
+class KnapsackProTestFileFinder {
+  private testFiles: TestFile[] = [];
+
+  public allTestFiles() {
+    glob("cypress/integration/**/*.js", {}, (error: any, files: any) => {
+      // files is an array of filenames.
+      // If the `nonull` option is set, and nothing
+      // was found, then files is ["**/*.js"]
+      // er is an error object or null.
+      if (error) {
+        // tslint:disable-next-line:no-console
+        console.error(error);
+        return;
+      }
+
+      files.forEach((testFilePath: string) => {
+        this.testFiles.push({ path: testFilePath });
+      });
+      console.log(this.testFiles); // here has value
+    });
+
+    console.log(this.testFiles); // here is empty, because of async operation
+    return this.testFiles;
+  }
+}
+
+const knapsackProTestFileFinder = new KnapsackProTestFileFinder();
+
+const knapsackPro = new KnapsackProCore(knapsackProTestFileFinder.allTestFiles());
 
 const onSuccess = async (queueTestFiles: TestFile[]) => {
   const recordedTestFiles: TestFile[] = [];
+
+  // TODO remove log, this should be list of tests fetched from queue
+  console.log(queueTestFiles);
 
   const deferredRecordedTestFiles = new Promise<TestFile[]>((resolve: any, reject: any) => {
     // https://docs.cypress.io/guides/guides/module-api.html#Example
