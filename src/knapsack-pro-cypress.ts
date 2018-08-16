ed532556ef721dc3c7886369d2ab5a4d58fe7a0a
Rafał Trzop <rafaltrzop@gmail.com>
2018-08-16 19:58:46 +0200

    Refactor onSuccess

--- src/knapsack-pro-cypress.ts
+++ src/knapsack-pro-cypress.ts
@@ -8,31 +8,26 @@ import { TestFilesFinder } from "./test-files-finder";
 const allTestFiles: TestFile[] = TestFilesFinder.allTestFiles();
 const knapsackPro = new KnapsackProCore(allTestFiles);
 
-const onSuccess = async (queueTestFiles: TestFile[]) => {
+const onSuccess = (queueTestFiles: TestFile[]): Promise<TestFile[]> => {
   const testFilePaths: string[] = queueTestFiles.map((testFile: TestFile) => testFile.path);
   const recordedTestFiles: TestFile[] = [];
 
-  const deferredRecordedTestFiles = new Promise<TestFile[]>((resolve: any, reject: any) => {
+  return new Promise<TestFile[]>((resolve: any, reject: any) => {
     // https://docs.cypress.io/guides/guides/module-api.html#Example
     cypress
-      .run({
-        spec: testFilePaths,
-      })
+      .run({ spec: testFilePaths })
       .then(({ runs: tests }: { runs: object[] }) => {
         tests.forEach((test: any) => {
-          const timeExecutionMs = test.stats.wallClockDuration; // in miliseconds
-          const timeExecution = timeExecutionMs / 1000.0;
+          const timeExecutionSeconds = test.stats.wallClockDuration / 1000;
           recordedTestFiles.push({
             path: test.spec.relative,
-            time_execution: timeExecution,
+            time_execution: timeExecutionSeconds,
           });
         });
 
         resolve(recordedTestFiles);
       });
   });
-
-  return deferredRecordedTestFiles;
 };
 
 const onError = (error: any) => {
