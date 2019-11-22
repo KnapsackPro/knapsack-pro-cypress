c36bbf365789a9dfa57ee5a0f341a01b5b658496
Artur Trzop <arturtrzop@gmail.com>
2019-11-22 14:04:39 +0100

    Run tests only when they are detected on the disk. Do not run tests when the test file pattern is invalid. Require to set KNAPSACK_PRO_TEST_FILE_PATTERN

--- src/test-files-finder.ts
+++ src/test-files-finder.ts
@@ -1,12 +1,25 @@
 import glob = require('glob');
 
-import { TestFile } from '@knapsack-pro/core';
+import { KnapsackProLogger, TestFile } from '@knapsack-pro/core';
 import { EnvConfig } from './env-config';
 
 export class TestFilesFinder {
   public static allTestFiles(): TestFile[] {
-    return glob
+    const testFiles = glob
       .sync(EnvConfig.testFilePattern)
       .map((testFilePath: string) => ({ path: testFilePath }));
+
+    if (testFiles.length === 0) {
+      const knapsackProLogger = new KnapsackProLogger();
+
+      const errorMessage =
+        // tslint:disable-next-line: max-line-length
+        'Test files cannot be found.\nPlease set KNAPSACK_PRO_TEST_FILE_PATTERN matching your test directory structure.\nLearn more: https://github.com/KnapsackPro/knapsack-pro-cypress#how-to-run-tests-only-from-specific-directory';
+
+      knapsackProLogger.error(errorMessage);
+      throw errorMessage;
+    }
+
+    return testFiles;
   }
 }
