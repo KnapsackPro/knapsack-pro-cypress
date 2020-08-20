016b4ffdd247317c7a32903de91c6bd95ac6927f
Artur Trzop <arturtrzop@gmail.com>
2020-08-20 15:11:39 +0200

    Use Cypress 5.0.0 and adjsut code to detect test file time properly for cypress 5.x

--- src/knapsack-pro-cypress.ts
+++ src/knapsack-pro-cypress.ts
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 
-const { name: clientName, version: clientVersion } = require('../package.json');
+const { name: clientName, version: clientVersion } = require("../package.json");
 
-const cypress = require('cypress');
+const cypress = require("cypress");
 
 import {
   KnapsackProCore,
   KnapsackProLogger,
   onQueueFailureType,
   onQueueSuccessType,
-  TestFile,
-} from '@knapsack-pro/core';
-import { EnvConfig } from './env-config';
-import { TestFilesFinder } from './test-files-finder';
-import { CypressCLI } from './cypress-cli';
+  TestFile
+} from "@knapsack-pro/core";
+import { EnvConfig } from "./env-config";
+import { TestFilesFinder } from "./test-files-finder";
+import { CypressCLI } from "./cypress-cli";
 
 const cypressCLIOptions = CypressCLI.argvToOptions();
 const knapsackProLogger = new KnapsackProLogger();
 knapsackProLogger.debug(
-  `Cypress CLI options:\n${KnapsackProLogger.objectInspect(cypressCLIOptions)}`,
+  `Cypress CLI options:\n${KnapsackProLogger.objectInspect(cypressCLIOptions)}`
 );
 
 EnvConfig.loadEnvironmentVariables();
@@ -27,34 +27,37 @@ const allTestFiles: TestFile[] = TestFilesFinder.allTestFiles();
 const knapsackPro = new KnapsackProCore(
   clientName,
   clientVersion,
-  allTestFiles,
+  allTestFiles
 );
 
 const onSuccess: onQueueSuccessType = async (queueTestFiles: TestFile[]) => {
   const testFilePaths: string[] = queueTestFiles.map(
-    (testFile: TestFile) => testFile.path,
+    (testFile: TestFile) => testFile.path
   );
   const { runs: tests, totalFailed } = await cypress.run({
     ...cypressCLIOptions,
-    spec: testFilePaths,
+    spec: testFilePaths
   });
 
   // when Cypress crashed
-  if (typeof tests === 'undefined') {
+  if (typeof tests === "undefined") {
     return {
       recordedTestFiles: [],
-      isTestSuiteGreen: false,
+      isTestSuiteGreen: false
     };
   }
 
   const recordedTestFiles: TestFile[] = tests.map((test: any) => ({
     path: test.spec.relative,
-    time_execution: test.stats.wallClockDuration / 1000,
+    time_execution:
+      // test.stats.wallClockDuration - Cypress 3.x and 4.x
+      // test.stats.duration - Cypress 5.x
+      (test.stats.wallClockDuration || test.stats.duration) / 1000
   }));
 
   return {
     recordedTestFiles,
-    isTestSuiteGreen: totalFailed === 0,
+    isTestSuiteGreen: totalFailed === 0
   };
 };
 
